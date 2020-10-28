4aad9357322f8d68322a3ab13bd9db7acf51fb92
Justin Chiou <chiou.kai@gmail.com>
2020-10-28 19:27:34 -0400

    add: vue-i18n to packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
+import VueI18n from "vue-i18n";
 import App from "./App.vue";
 import store from "./store";
 
 createApp(App)
   .use(store)
+  .use(VueI18n)
   .mount("#app");
