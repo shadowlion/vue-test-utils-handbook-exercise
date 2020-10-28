a5dce6f2a3323526c27ee08ebd157bc248f3686e
Justin Chiou <chiou.kai@gmail.com>
2020-10-28 13:45:00 -0400

    add: Form Submitter component/tests

---
+++ src/components/FormSubmitter/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input v-model="username" data-username />
+      <input type="submit" />
+    </form>
+
+    <div class="message" v-if="submitted">
+      Thank you for your submission, {{ username }}.
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "FormSubmitter",
+
+  setup() {
+    const username = ref("");
+    const submitted = ref(false);
+
+    function handleSubmit() {
+      submitted.value = true;
+    }
+
+    return { username, submitted, handleSubmit };
+  },
+});
+</script>
