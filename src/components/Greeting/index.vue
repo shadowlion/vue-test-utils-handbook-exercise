85b46d0c8cfc84620fdf9059ba37289564ed0dca
Justin Chiou <chiou.kai@gmail.com>
2020-10-28 10:30:11 -0400

    feat: add/test Greeting component

---
+++ src/components/Greeting/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ greeting }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Greeting",
+  setup() {
+    const greeting = ref("Vue and TDD");
+    return { greeting };
+  },
+});
+</script>
